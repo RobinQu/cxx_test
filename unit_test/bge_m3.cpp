2fb3f9fa1f530387702df1bbf4b7bbec49e579bd
RobinQu <robinqu@gmail.com>
2024-05-23 14:02:14 +0000

    feat: model layers

--- unit_test/bge_m3.cpp
+++ unit_test/bge_m3.cpp
@@ -3,17 +3,112 @@
 //
 #include <ggml.h>
 #include <vector>
+#include <iostream>
 
-class Activation {
-
+enum ActFunc {
+    GELU,
+    SILU,
+    Tanh,
+    RELU,
+    RELU2
 };
 
-class Linear {
-    ggml_tensor *weight__;
-    ggml_tensor *bias__;
+ggml_tensor *inplace_act(ggml_context *ctx, ActFunc act, ggml_tensor *input)
+{
+    switch (act)
+    {
+        case ActFunc::GELU:
+            return ggml_gelu_inplace(ctx, input);
+        case ActFunc::SILU:
+            return ggml_silu_inplace(ctx, input);
+        case ActFunc::Tanh:
+            return ggml_tanh_inplace(ctx, input);
+        case ActFunc::RELU:
+            return ggml_relu_inplace(ctx, input);
+        case ActFunc::RELU2:
+        {
+            ggml_tensor *output = ggml_relu_inplace(ctx, input);
+            output = ggml_sqr_inplace(ctx, output);
+            return output;
+        }
+        default:
+            std::cerr << "not implemented act function: " << act;
+            return nullptr;
+    }
+}
+
+
+struct ForwardContext {
+    ggml_context* g_ctx = nullptr;
+    ggml_cgraph* g_cgraph = nullptr;
+    ggml_scratch g_scratch {};
+    virtual ~ForwardContext() {
+        ggml_free(g_ctx);
+    }
 };
 
-class LayerNorm {
+struct InitContext {
+    ggml_context* g_ctx;
+    ggml_type dtype;
+};
+
+class Block {
+public:
+    explicit Block(ggml_prec precision = GGML_PREC_DEFAULT, int id = 0) : precision_(precision), id_(id) {}
+    virtual ~Block() = default;
+    virtual ggml_tensor* Forward(ForwardContext* ctx, ggml_tensor* input) {
+        throw std::runtime_error("Not implemented");
+    }
+    virtual ggml_tensor* Forward(ForwardContext* ctx, ggml_tensor* input, int n_past) {
+        throw std::runtime_error("Not implemented");
+    };
+protected:
+    ggml_prec precision_;
+    int id_;
+};
+
+class Linear: public Block {
+public:
+    Linear(ggml_tensor *weight, ggml_tensor *bias) : weight_(weight), bias_(bias) {}
+
+    Linear(InitContext* init_context ,int in_features, int out_features, bool use_bias):
+        weight_(ggml_new_tensor_2d(init_context->g_ctx, init_context->dtype, in_features, out_features)),
+        bias_(use_bias ? ggml_new_tensor_1d(init_context->g_ctx, GGML_TYPE_F32, out_features) : nullptr)
+    {}
+
+    ggml_tensor *Forward(ForwardContext *ctx, ggml_tensor *input) override {
+        ggml_tensor *output = ggml_mul_mat(ctx->g_ctx, weight_, input); // [seqlen, out_features]
+        ggml_mul_mat_set_prec(output, precision_);
+        if (bias_)
+        {
+            output = ggml_add_inplace(ctx->g_ctx, output, bias_);
+        }
+        return output;
+    }
+private:
+    ggml_tensor *weight_;
+    ggml_tensor *bias_;
+};
+
+class LayerNorm: public Block {
+public:
+    LayerNorm(ggml_tensor *weight, ggml_tensor *bias) : weight_(weight), bias_(bias), eps_(1e-5f) {}
+    LayerNorm(InitContext* init_context, int normalized_shape, bool use_bias = true):
+        weight_(ggml_new_tensor_1d(init_context->g_ctx, GGML_TYPE_F32, normalized_shape)),
+        bias_(use_bias ? ggml_new_tensor_1d(init_context->g_ctx, GGML_TYPE_F32, normalized_shape) : nullptr),
+        eps_(1e-5)
+    {}
+
+    ggml_tensor *Forward(ForwardContext *ctx, ggml_tensor *input) override {
+        // input: [seqlen, normalized_shape]
+        ggml_tensor *output = ggml_norm_inplace(ctx->g_ctx, input, eps_);
+        output = ggml_mul_inplace(ctx->g_ctx, output, weight_);
+        if (bias_)
+            output = ggml_add_inplace(ctx->g_ctx, output, bias_);
+        return output;
+    }
+
+private:
     ggml_tensor* weight_;
     ggml_tensor* bias_;
     float eps_;
@@ -31,7 +126,7 @@ class RobertaSelfAttention {
 
 };
 
-class RebertaMLP {
+class RobertaMLP {
     Linear dense_;
     LayerNorm norm_;
 };
@@ -40,14 +135,14 @@ class RebertaMLP {
 class RobertaBlock {
     RobertaSelfAttention attention_;
     LayerNorm post_attention_layer_norm__;
-    RebertaMLP mlp_;
+    RobertaMLP mlp_;
     LayerNorm output_layer_norm_;
 };
 
 
 class RobertaClassificationHead {
     Linear dense_;
-    Activation activation_;
+    ActFunc activation_;
     Linear out_proj_;
 };
 
@@ -57,7 +152,14 @@ class XLMRoberta {
     RobertaClassificationHead final_;
 };
 
-class BGEM3Model {
+struct GenerationConfig {};
+
+class BGEM3RerankerModel {
+public:
+    float GetScore(const GenerationConfig& config,  const std::vector<int>& input_tokens) {
+
+    }
+private:
     XLMRoberta roberta_;
 };
 
